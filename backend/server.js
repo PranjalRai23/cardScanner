d9f0f61cd9c91dbeeb6074b162909b6514ffd670
Pranjal Rai <pranjalrai1@gmail.com>
2024-10-07 05:03:04 +0530

    Deployment commit

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-import path from "path";
+import path from 'path';
 import express from "express";
 import dotenv from "dotenv";
 
@@ -6,22 +6,22 @@ import { connectDB } from "./db/db.js";
 
 import cardRoute from "./routes/card.routes.js";
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
-
 app.use(express.json());
 
 app.use("/api/v1", cardRoute);
 
-app.use(express.static(path.join(__dirname,"/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 })
 
 app.listen(PORT, () => {
